ff68a8772e323b64487201ebcbd966ca2dcb1d5e
liao1565553 <949395746@qq.com>
2017-04-10 22:51:30 +0800

    终于解决了包重构的问题了

---
+++ renren-web/src/main/java/io/renren/system/dao/SysOssDao.java
@@ -0,0 +1,15 @@
+package io.renren.system.dao;
+
+import io.renren.dao.BaseDao;
+import io.renren.system.entity.SysOssEntity;
+
+/**
+ * 文件上传
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-25 12:13:26
+ */
+public interface SysOssDao extends BaseDao<SysOssEntity> {
+	
+}
